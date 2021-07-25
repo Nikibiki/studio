bc76b02dcc852e9a6e523c5dbadba64d1b56a9a2
Nikita <nik-kilin@mail.ru>
2021-07-25 18:24:06 +0700

    create_student_groupe_course_with_teacher_migration

---
+++ migrations/m210725_110427_create_student_groupe_course_with_teacher_table.php
@@ -0,0 +1,60 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%student_groupe_course_with_teacher}}`.
+ */
+class m210725_110427_create_student_groupe_course_with_teacher_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%student_groupe_course_with_teacher}}', [
+            'id' => $this->primaryKey(),
+            'group_id' => $this->integer()->notNull(),
+            'course_id' => $this->integer()->notNull(),
+            'teacher_id' => $this->integer()->notNull(),
+        ]);
+
+        $this->addForeignKey(
+            'fk_table-course_id',
+            'student_groupe_course_with_teacher',
+            'group_id',
+            'course',
+            'id'
+        );
+
+        $this->addForeignKey(
+            'fk_table-teacher_id',
+            'student_groupe_course_with_teacher',
+            'teacher_id',
+            'teacher',
+            'id'
+        );
+
+
+
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey(
+            'fk_table-teacher_id',
+            'student_groupe_course_with_teacher'
+        );
+
+        $this->dropForeignKey(
+            'fk_table-course_id',
+            'student_groupe_course_with_teacher'
+        );
+
+        $this->dropTable('{{%student_groupe_course_with_teacher}}');
+    }
+}
