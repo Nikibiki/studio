5839bb9ea98b76a7d860e0fdcd8e7c012bbd26b8
Nikita <nik-kilin@mail.ru>
2021-07-25 17:27:59 +0700

    create_student_groupe_migration

---
+++ migrations/m210725_102043_create_student_groupe_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%student_groupe}}`.
+ */
+class m210725_102043_create_student_groupe_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%student_groupe}}', [
+            'id' => $this->primaryKey(),
+            'groupe_id' => $this->integer()->notNull(),
+            'student_id' => $this->integer()->notNull()
+        ]);
+
+        
+        $this->createIndex(
+            'idx-student_groupe-student_id',
+            'student_groupe',
+            'student_id'
+        );
+
+        $this->addForeignKey(
+            'fk_student_groupe-student_id',
+            'student_groupe',
+            'student_id',
+            'student',
+            'id'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%student_groupe}}');
+    }
+}
