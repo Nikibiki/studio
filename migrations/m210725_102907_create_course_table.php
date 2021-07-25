5b412fc1383117e183a3300074b6475071faeae0
Nikita <nik-kilin@mail.ru>
2021-07-25 17:30:04 +0700

    create_course_migration

---
+++ migrations/m210725_102907_create_course_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%course}}`.
+ */
+class m210725_102907_create_course_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%course}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%course}}');
+    }
+}
